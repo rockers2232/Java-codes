85668cda91d29a98cd9b2e051e7c50b93d5d92c0
rockers2232 <ayushsainisaa2806@gmail.com>
2025-02-22 10:52:21 +0530

    hello

---
+++ countarr.java
@@ -0,0 +1,41 @@
+
+public class countarr {
+    public static void main(String[] args) {
+        int arr[]={1,5,1,2,2,5,3};
+        boolean visit[]=new boolean[arr.length];
+        for(int i=0;i<arr.length;i++)
+        {
+            int c=1;
+              if(visit[i])
+                continue;
+            for(int j=i+1;j<arr.length;j++)
+            {
+                
+                    if(arr[i]==arr[j])
+                    {
+                        c++;
+                        visit[j]=true;
+                    }
+                
+            }
+            
+            System.out.println(arr[i]+"="+c);
+        }
+        int l=arr[0];
+        int sl=arr[0];
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>l)
+            {
+                sl=l;
+                l=arr[i];
+            }
+            else if(arr[i]>sl&&arr[i]!=l)
+            {
+                sl=arr[i];
+            }
+        }
+        System.out.println(sl);
+    }
+    
+}
